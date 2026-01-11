4b5f5546b36902e224870487bf96a2d5ad6b2da8
Brandly123 <FlightlessBird0604@gmail.com>
2026-01-10 18:47:02 -0800

    Update for stress testing 2

--- script.js
+++ script.js
@@ -7,27 +7,38 @@ let done = 0;
 
 let best = 0;
 let tickdelay = 3;
+let stressamount = 0;
 
 function collatzStep() {
-    ip *= 1.01;
-    ip += 1;
     const maxIterations = Math.floor(ip);
     let iterations = 0;
     
     const timeCheck = performance.now() - start;
     start = performance.now();
     tickdelay --;
-    if (tickdelay > 0 || (timeCheck <= 50 && done <= 3)) {
-        document.getElementById("calculated").innerHTML = 
-            `#${done+1}: ${timeCheck.toFixed(2)}ms/50ms stress; stress magnitude of ${Math.log10(ip ** 0.8).toFixed(2)}`;
+
+    if(timeCheck > 40 || stressamount){
+        ip *= 1.001;
+    } else if(timeCheck > 20){
+        ip *= 1.01;
     } else {
+        ip += 1;
+        ip *= 1.03;
+    }
+    stressamount++;
+    if (tickdelay > 0 || (timeCheck <= 30 + done*40 && done <= 1)) stressamount --;
+    if (stressamount < 10) {
+        document.getElementById("calculated").innerHTML = 
+            `#${done+1}: ${timeCheck.toFixed(2)}ms/${30 + done*40}ms stress; stress magnitude of ${Math.log10(ip ** 0.75).toFixed(2)} (exceeded stress frames: ${stressamount}/10)`;
+    } else {
+        stressamount = 0;
             best += ip;
             done += 1;
         ip /= 2;
         tickdelay = 2;
 
         document.getElementById("calculated").innerHTML = 
-            ((best/4)**0.4).toLocaleString() + ` Score`;
+            ((best/2)**0.5).toLocaleString() + ` Score`;
     }
     
     while (iterations < maxIterations) {
@@ -41,7 +52,7 @@ function collatzStep() {
         iterations++;
     }
 
-    if(done <= 3) requestAnimationFrame(collatzStep);
+    if(done <= 1) requestAnimationFrame(collatzStep);
 }
 
 requestAnimationFrame(collatzStep);
